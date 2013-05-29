340ccf9411e507ea109677ef32da609060f99084
Vladimir Ipatov <vs@ipatov.net>
2013-05-30 02:32:19 +0400

    add iptables autoconfig support

--- profiles/cbs.files/postinst.sh
+++ profiles/cbs.files/postinst.sh
@@ -188,8 +188,9 @@ fi
 ## set cbs apt sources
 cp files/apt/sci-dev.list files/apt/apt.pub $target/etc/apt/sources.list.d
 cp files/apt/apt.pub $target/etc/apt
-echo "deb http://mirror.yandex.ru/debian/ wheezy main contrib non-free \
-deb http://mirror.yandex.ru/debian-security/ wheezy/updates main contrib non-free" >> $target/etc/apt/sources.list
+echo "deb http://mirror.yandex.ru/debian/ wheezy main" >> $target/etc/apt/sources.list
+echo "deb http://mirror.yandex.ru/debian-security/ wheezy/updates main" >> $target/etc/apt/sources.list
+echo "deb http://ftp.debian.org/debian-backports/ wheezy-backports main" >> $target/etc/apt/sources.list
 chroot $target "apt-key add /etc/apt/apt.pub"
 
 ## Add cbs deploing scripts
